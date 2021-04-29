419289ecde8b634e71604b5e3779a9ef6798bce2
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-04-29 01:47:15 -0500

    Import layout into /book/ page. Remove default footer

--- src/templates/bookTemplate.js
+++ src/templates/bookTemplate.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from "../components/layout"
 
 const BookTemplate = () => {
     return(
-        <div>
+        <Layout>
           Book page  
-        </div>
+        </Layout>
     )
 
 }
