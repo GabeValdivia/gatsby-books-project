c40aa3d102d78b836881cf225cc226e099398724
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-04-29 02:09:16 -0500

    Add some styles to bookTemplate.js page. Make sure styles work

--- src/templates/bookTemplate.js
+++ src/templates/bookTemplate.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const BookItem = styled.section`
+  border: 3px solid red;
+
+  h2{
+    small {
+      font-weight: normal;
+    }
+  }
+`;
 
 const BookTemplate = (props) => {
     return(
         <Layout>
-          <section>
+          <BookItem>
             <h2>
               {props.pageContext.title} - <small>{props.pageContext.author.name}</small>
             </h2>
              <p>{props.pageContext.summary}</p>
-          </section>
+          </BookItem>
         </Layout>
     )
 
