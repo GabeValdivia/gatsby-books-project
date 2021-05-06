fcb927647f0baef91bed290f61d9464e6ee8479d
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-06 03:31:37 -0500

    Render user.email on header when logged in

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,47 @@
-import * as React from "react";
-import PropTypes from "prop-types";
 import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React, {useContext} from "react";
 import {FirebaseContext} from "./Firebase";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const {firebase, user} = useContext(FirebaseContext);
+  console.log(firebase, user);
+  
+    return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-      <FirebaseContext.Consumer>
-        {props => {
-          console.log(props);
-          return <div/>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
         }}
-      </FirebaseContext.Consumer>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <div>
+          {!!user && !!user.email && 
+            <div>
+              Hello, {user.email}
+            </div>
+          }
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
