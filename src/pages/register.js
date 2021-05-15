f5de0bf4fa40390de07d971fd33837511da3929b
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-15 02:31:42 -0500

    Create index.js move Form, Input Button and import to register.js and login.js. Create simple login from. Set up handleSubmit()

---
+++ src/pages/register.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Form, Input, Button } from '../components/common';
+
+
+const Register = () => {
+    function handleSubmit(e) {
+        e.preventDefaut();
+    }
+    return(
+        <Form onSubmit={handleSubmit}>
+            <Input placeholder="email" type="email" required />
+            <Input placeholder="password" type="password" required minLength={3}/>
+            <Input placeholder="confirm password" type="password" required minLength={3} />
+            <Button type="submit" block>
+                Register
+            </Button>
+        </Form>
+    )
+}
+
+
+export default Register
