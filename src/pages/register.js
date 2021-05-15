7d3dfa1e0fde60e2cb535a57615bcf586a71dedc
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-15 02:47:22 -0500

    Set up handleChangeSubmit, prevenDefault, test

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Input, Button } from '../components/common';
 
 
 const Register = () => {
+
+    const [fromValues, setFormValues] = useState({
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    function handleInputChange(e) {
+        e.persist();
+        setFormValues(currentValues => ({
+            ...currentValues,
+            [e.target.name]: e.target.value
+        }))
+    }
+
     function handleSubmit(e) {
-        e.preventDefaut();
+        e.preventDefault();
+
+        console.log(fromValues);
     }
     return(
         <Form onSubmit={handleSubmit}>
-            <Input placeholder="email" type="email" required />
-            <Input placeholder="password" type="password" required minLength={3}/>
-            <Input placeholder="confirm password" type="password" required minLength={3} />
+            <Input onChange={handleInputChange} value={fromValues.email} placeholder="email" type="email" required name="email"/>
+            <Input onChange={handleInputChange} value={fromValues.password} placeholder="password" type="password" required minLength={3} name="password"/>
+            <Input onChange={handleInputChange} value={fromValues.confirmPassword} placeholder="confirm password" type="password" required minLength={3} name="confirmPassword"/>
             <Button type="submit" block>
                 Register
             </Button>
