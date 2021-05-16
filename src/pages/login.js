3e1413bef4ecf55d09f9dbea57bcf71f5628e3ef
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-16 00:03:53 -0500

    Add errorMessage function to display error message when user enters wrong email or password

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,15 +7,20 @@ const Login = () => {
 
     const [formValues, setFormValues] = useState({email: '', password: ''});
     const {firebase} = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('');
     
     function handleSubmit(e) {
         e.preventDefault();
 
-        firebase.login({email: formValues.email, password: formValues.password});
+        firebase.login({email: formValues.email, password: formValues.password}).catch(error => {
+            console.log(error);
+            setErrorMessage(error.message);
+        });
     }
 
     function handleInputChange(e) {
         e.persist();
+        setErrorMessage('');
         setFormValues(currentValues => ({
             ...currentValues,
             [e.target.name]: e.target.value
@@ -25,8 +30,13 @@ const Login = () => {
     return(
         <section>
             <Form onSubmit={handleSubmit}>
-                <Input value={formValues.email } name="email" onChange={handleInputChange} placeholder="email" type="email" />
-                <Input value={ formValues.password} name="password" onChange={handleInputChange} placeholder="password" type="password" />
+                <Input required value={formValues.email } name="email" onChange={handleInputChange} placeholder="email" type="email" />
+                <Input required value={ formValues.password} name="password" onChange={handleInputChange} placeholder="password" type="password" />
+                {!!errorMessage &&
+                    <span>
+                        {errorMessage}
+                    </span>
+                }
                 <Button type="submit" block>
                     Login
                 </Button>
