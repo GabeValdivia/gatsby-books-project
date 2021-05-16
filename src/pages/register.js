868ef63d6d62c589d2ce46a584a094a3b08f2ad1
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-05-16 00:37:37 -0500

    Implement ErrorMessage validation on registration from passwords

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,10 +1,11 @@
 import React, {useState, useContext} from 'react';
-import { Form, Input, Button } from '../components/common';
+import { Form, Input, Button, ErrorMessage } from '../components/common';
 import {FirebaseContext} from '../components/Firebase';
 
 
 const Register = () => {
     const {firebase} = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const [formValues, setFormValues] = useState({
         email: '',
@@ -14,6 +15,7 @@ const Register = () => {
 
     function handleInputChange(e) {
         e.persist();
+        setErrorMessage('');
         setFormValues(currentValues => ({
             ...currentValues,
             [e.target.name]: e.target.value
@@ -27,16 +29,23 @@ const Register = () => {
             firebase.register({
                 email: formValues.email,
                 password: formValues.password
+            }).catch(error => {
+                setErrorMessage(error.message);
             })
+        }else{
+            setErrorMessage('Password and Confirm Password fields must match')
         }
-
-        console.log(formValues);
     }
     return(
         <Form onSubmit={handleSubmit}>
             <Input onChange={handleInputChange} value={formValues.email} placeholder="email" type="email" required name="email"/>
             <Input onChange={handleInputChange} value={formValues.password} placeholder="password" type="password" required minLength={3} name="password"/>
             <Input onChange={handleInputChange} value={formValues.confirmPassword} placeholder="confirm password" type="password" required minLength={3} name="confirmPassword"/>
+            {!!errorMessage &&
+                <ErrorMessage>
+                    {errorMessage}
+                </ErrorMessage>
+            }
             <Button type="submit" block>
                 Register
             </Button>
